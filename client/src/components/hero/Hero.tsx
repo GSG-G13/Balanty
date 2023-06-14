5fe6d937f26625d8a80a71c3818b894a4695dacb
unknown <ahmedirheem2003@gmail.com>
2023-06-14 19:41:58 +0300

    build: build Hero component

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -9,19 +9,17 @@ interface Props {
 
 const Hero: FC<Props> = ({ image, type }) => {
   return (
-    <Wrapper>
+    <Wrapper imageUrl={image}>
       {type === 'ملعب' && (
         <HeroDescription
           title="روَج لملعبك الخاص"
           description="ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب"
-          type={type}
         />
       )}
       {type === 'لاعب' && (
         <HeroDescription
           title="احجز المباريات ونافس"
           description="ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب ملعب"
-          type={type}
         />
       )}
       <JoinButton>إنضم ك{type}</JoinButton>
